0df117b769356db17ddfde0f21728fec8a8a39a4
yumewebs <71258059+yumewebs@users.noreply.github.com>
2020-10-07 20:33:03 +0200

    Add files via upload
    Remember that you need to add different node_modules for it

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var params = process.argv.slice(2);
+
+var numero1 = parseFloat(params[0]);
+var numero2 = parseFloat(params[1]);
+
+var plantilla = `
+La suma es: ${numero1 + numero2}
+La resta es: ${numero1 - numero2}
+La multiplicación es: ${numero1*numero2}
+La división es: ${numero1/numero2}
+
+`;
+
+console.log(plantilla);
+
+console.log("Hola mundo con NodeJS");
